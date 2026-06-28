20110fb155376c565d3a03e4492c3301ba00af3d
saurabhku2k2-byte <saurabhku2k2@gmail.com>
2026-06-28 19:58:13 +0530

    Update Q_01.sql

--- Introduction to RDMS/Mini Project/Normalization/Q_01.sql
+++ Introduction to RDMS/Mini Project/Normalization/Q_01.sql
@@ -1,3 +1,15 @@
+CREATE TABLE Member (
+    Member_Id INT,
+    First_Name VARCHAR(50),
+    Last_Name VARCHAR(50),
+    Hobbies VARCHAR(200)
+);
+
+INSERT INTO Member VALUES
+(101, 'Jayson', 'Mark', 'Cricket,Swimming,Football'),
+(102, 'Ram', 'Ganesh', 'Swimming,Running,Music'),
+(103, 'Raj', 'Kishore', 'Dancing,Singing,Running');
+
 CREATE TABLE Member_Hobbies (
     Member_Id INT,
     First_Name VARCHAR(50),
@@ -6,14 +18,12 @@ CREATE TABLE Member_Hobbies (
 );
 
 INSERT INTO Member_Hobbies VALUES
-(101,'Jayson','Mark','Cricket'),
-(101,'Jayson','Mark','Swimming'),
-(101,'Jayson','Mark','Football'),
-
-(102,'Ram','Ganesh','Swimming'),
-(102,'Ram','Ganesh','Running'),
-(102,'Ram','Ganesh','Music'),
-
-(103,'Raj','Kishore','Dancing'),
-(103,'Raj','Kishore','Singing'),
-(103,'Raj','Kishore','Running');
+(101, 'Jayson', 'Mark', 'Cricket'),
+(101, 'Jayson', 'Mark', 'Swimming'),
+(101, 'Jayson', 'Mark', 'Football'),
+(102, 'Ram', 'Ganesh', 'Swimming'),
+(102, 'Ram', 'Ganesh', 'Running'),
+(102, 'Ram', 'Ganesh', 'Music'),
+(103, 'Raj', 'Kishore', 'Dancing'),
+(103, 'Raj', 'Kishore', 'Singing'),
+(103, 'Raj', 'Kishore', 'Running');
